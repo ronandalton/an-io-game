6ba063e0b90369cbe0a4b66cbcc735f3453d7f58
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2023-04-18 21:52:56 +1200

    Refactor everything

--- game-server.js
+++ game-server.js
@@ -1,77 +1,262 @@
 import {WebSocketServer} from 'ws';
 
-const wss = new WebSocketServer({port: 4000});
+
+const SERVER_PORT = 4000;
+const MAX_PLAYERS = 200;
+const GAME_AREA_WIDTH = 500;
+const GAME_AREA_HEIGHT = 400;
+const SERVER_TICK_PERIOD = 40; // in milliseconds
+const CELL_MOVEMENT_SPEED = 20;
 
 
-wss.on('connection', (ws) => {
-	ws.on('error', console.error);
+let websocketServer = null;
+const clientStates = new Map(); // key: clientId (all client data except player specific data)
+const players = new Map(); // key: playerId
+const cells = new Map(); // key: cellId
+const availablePlayerIds = [];
 
-	ws.on('message', (data) => {
-		//console.log('received: %s', data);
-		try {
-			const message = JSON.parse(data);
-
-			switch (message.type) {
-				case "targetPositionUpdate":
-					targetX = message.targetX;
-					targetY = message.targetY;
-					break;
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	});
-});
+for (let i = MAX_PLAYERS - 1; i >= 0; i--) {
+	availablePlayerIds.push(i);
+}
 
 
-class Cell {
-	constructor(x, y, radius, color) {
+class Position {
+	constructor(x, y) {
 		this.x = x;
 		this.y = y;
-		this.radius = radius;
-		this.color = color;
 	}
 }
 
 
-const playerCell = new Cell(50, 50, 30, "#0095DD");
-let targetX = 50;
-let targetY = 50;
+class Cell {
+	constructor(id, position) {
+		this.id = id;
+		this.position = position;
+	}
+}
 
 
-function moveCellTowardsTarget(cell, targetX, targetY, moveDistance) {
-	const dx = targetX - cell.x;
-	const dy = targetY - cell.y;
+class Player {
+	constructor(id, cellId, targetPosition) {
+		this.id = id;
+		this.cellId = cellId;
+		this.targetPosition = targetPosition;
+	}
+}
+
+
+class ClientState {
+	constructor(clientId) {
+		this.clientId = clientId;
+		this.playerId = null; // null if dead or spectating
+	}
+}
+
+
+function startServer() {
+	setupGame();
+
+	websocketServer = new WebSocketServer({port: SERVER_PORT});
+	websocketServer.on('connection', handleNewConnection);
+
+	setInterval(tick, SERVER_TICK_PERIOD);
+}
+
+
+function setupGame() {
+	// TODO
+}
+
+
+function handleNewConnection(connection) {
+	registerNewClient(connection);
+
+	connection.on('error', console.error);
+	connection.on('message', (data) => handleWebsocketMessage(connection, data));
+}
+
+
+function registerNewClient(connection) {
+	const clientId = generateClientId();
+
+	const clientState = new ClientState(clientId);
+	clientStates.set(clientId, clientState);
+
+	connection.clientId = clientId;
+}
+
+
+function spawnPlayer() {
+	const playerId = getUnusedPlayerId();
+
+	if (playerId === null) {
+		return null; // can't spawn player because server is full
+	}
+
+	const cellId = generateGameObjectId();
+	const position = getRandomPosition();
+	const cell = new Cell(cellId, position);
+	cells.set(cellId, cell);
+
+	const player = new Player(playerId, cellId, null);
+	players.set(playerId, player);
+
+	return player;
+}
+
+
+function getUnusedPlayerId() {
+	if (availablePlayerIds.length > 0) {
+		return availablePlayerIds.pop();
+	} else {
+		return null;
+	}
+}
+
+
+function handleWebsocketMessage(connection, data) {
+	try {
+		const message = JSON.parse(data);
+
+		switch (message.type) {
+			case "joinGameRequest":
+				handleJoinGameRequestMessage(connection, message);
+				break;
+			case "targetPositionUpdate":
+				handleTargetPositionUpdateMessage(connection, message);
+				break;
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+
+function handleJoinGameRequestMessage(connection, message) {
+	const clientState = clientStates.get(connection.clientId);
+
+	if (clientState.playerId !== null) { // can't join game since already joined
+		sendJoinGameResponseMessage(connection, false);
+		return;
+	}
+
+	const player = spawnPlayer();
+
+	if (player === null) { // failed to spawn player (server full)
+		sendJoinGameResponseMessage(connection, false);
+		return;
+	}
+
+	clientState.playerId = player.id;
+
+	sendJoinGameResponseMessage(connection, true);
+}
+
+
+function handleTargetPositionUpdateMessage(connection, message) {
+	const playerId = clientStates.get(connection.clientId).playerId;
+
+	if (playerId === null) {
+		return; // message is meaningless if they aren't an actual player
+	}
+
+	const player = players.get(playerId);
+
+	player.targetPosition = message.position;
+}
+
+
+function sendJoinGameResponseMessage(connection, joinSuccessful) {
+	const clientState = clientStates.get(connection.clientId);
+
+	const message = {
+		type: "joinGameResponse",
+		joinSuccessful: joinSuccessful,
+		playerId: clientState.playerId
+	};
+
+	connection.send(JSON.stringify(message));
+}
+
+
+function updateGameState() {
+	for (const player of players.values()) {
+		const playerCell = cells.get(player.cellId);
+
+		if (player.targetPosition !== null) {
+			moveCellTowardsTargetPosition(playerCell, player.targetPosition, CELL_MOVEMENT_SPEED);
+		}
+	}
+}
+
+
+function moveCellTowardsTargetPosition(cell, targetPosition, moveDistance) {
+	const dx = targetPosition.x - cell.position.x;
+	const dy = targetPosition.y - cell.position.y;
 
 	const dist = Math.sqrt(dx * dx + dy * dy);
 
-	if (dist < moveDistance) {
-		cell.x = targetX;
-		cell.y = targetY;
+	if (dist <= moveDistance) {
+		cell.position.x = targetPosition.x;
+		cell.position.y = targetPosition.y;
 		return;
 	}
 
 	const dxNorm = dx / dist;
 	const dyNorm = dy / dist;
 
-	cell.x += dxNorm * moveDistance;
-	cell.y += dyNorm * moveDistance;
+	cell.position.x += dxNorm * moveDistance;
+	cell.position.y += dyNorm * moveDistance;
 }
 
 
-function tick() {
-	moveCellTowardsTarget(playerCell, targetX, targetY, 20);
-
+function constructClientGameUpdateMessage(clientId) {
 	const message = {
-		type: "cellUpdate",
-		cellX: playerCell.x,
-		cellY: playerCell.y
+		type: "gameUpdate",
+		cells: Array.from(cells.values())
 	};
 
-	wss.clients.forEach((client) => {
-		client.send(JSON.stringify(message));
+	return message;
+}
+
+
+function sendUpdatedGameStateToClients() {
+	websocketServer.clients.forEach((connection) => {
+		const message = constructClientGameUpdateMessage(connection.clientId);
+		connection.send(JSON.stringify(message));
 	});
 }
 
 
-setInterval(tick, 100);
+function tick() {
+	updateGameState();
+	sendUpdatedGameStateToClients();
+}
+
+
+function generateClientId() {
+	return getRandomInt(0, 2 ** 32);
+}
+
+
+function generateGameObjectId() {
+	return getRandomInt(0, 2 ** 32);
+}
+
+
+function getRandomPosition() {
+	const x = getRandomInt(0, GAME_AREA_WIDTH);
+	const y = getRandomInt(0, GAME_AREA_HEIGHT);
+
+	return new Position(x, y);
+}
+
+
+function getRandomInt(min, max) { // Note that min is inclusive and max is exclusive
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+startServer();
--- public/client.js
+++ public/client.js
@@ -1,43 +1,55 @@
+const SERVER_ADDRESS = "ws://localhost:4000";
+const DRAW_RATE = 60; // FPS
+const SERVER_TICK_PERIOD = 40; // in milliseconds
+const CELL_RADIUS = 50;
+const CELL_COLOR = "#0095DD";
+
+
 const canvas = document.getElementById("gameArea");
 const ctx = canvas.getContext("2d");
 
 
-class Cell {
-	constructor(x, y, radius, color) {
+let webSocket = null;
+let playerId = null;
+let playerTarget = null;
+const players = new Map(); // key: playerId
+const cells = new Map(); // key: cellId
+
+
+class Position {
+	constructor(x, y) {
 		this.x = x;
 		this.y = y;
-		this.radius = radius;
-		this.color = color;
 	}
 }
 
 
-function drawCell(cell) {
-	ctx.beginPath();
-	ctx.arc(cell.x, cell.y, cell.radius, 0, Math.PI * 2);
-	ctx.fillStyle = cell.color;
-	ctx.fill();
+class Cell {
+	constructor(id, position) {
+		this.id = id;
+		this.position = position;
+	}
 }
 
 
-const playerCell = new Cell(50, 50, 30, "#0095DD");
-let targetX = 50;
-let targetY = 50;
-
-
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawCell(playerCell);
+class Player {
+	constructor(id, cellId, targetPosition) {
+		this.id = id;
+		this.cellId = cellId;
+		this.targetPosition = targetPosition;
+	}
 }
 
 
-function updateTargetPosition(event) {
-	targetX = event.offsetX;
-	targetY = event.offsetY;
+function initCanvas() {
+	resizeCanvasToFitWindow();
+	window.addEventListener("resize", resizeCanvasToFitWindow);
+	document.addEventListener("mousemove", handleMouseMovedEvent);
+	setInterval(draw, 1000 / DRAW_RATE);
 }
 
 
-function resizeCanvas() {
+function resizeCanvasToFitWindow() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 
@@ -45,44 +57,92 @@ function resizeCanvas() {
 }
 
 
-document.addEventListener("mousemove", updateTargetPosition);
-window.addEventListener("resize", resizeCanvas);
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	for (const cell of cells.values()) {
+		drawCell(cell);
+	}
+}
 
 
-resizeCanvas();
+function drawCell(cell) {
+	ctx.beginPath();
+	ctx.arc(cell.position.x, cell.position.y, CELL_RADIUS, 0, Math.PI * 2);
+	ctx.fillStyle = CELL_COLOR;
+	ctx.fill();
+}
 
-setInterval(draw, 1000 / 100);
+
+function handleMouseMovedEvent(event) {
+	playerTarget = new Position(event.offsetX, event.offsetY);
+}
 
 
-const webSocket = new WebSocket("ws://localhost:4000");
+function connectToServer() {
+	webSocket = new WebSocket(SERVER_ADDRESS);
+
+	webSocket.addEventListener("open", handleWebSocketOpenedEvent);
+
+	webSocket.addEventListener("message", handleWebSocketMessageEvent);
+}
 
 
-function sendTargetPosition() {
+function handleWebSocketOpenedEvent(event) {
+	sendJoinGameRequest();
+}
+
+
+function sendJoinGameRequest() {
 	const message = {
-		type: "targetPositionUpdate",
-		targetX: targetX,
-		targetY: targetY
+		type: "joinGameRequest",
 	};
 
 	webSocket.send(JSON.stringify(message));
 }
 
 
-webSocket.onopen = (event) => {
-	setInterval(sendTargetPosition, 40);
-};
+function sendTargetPosition() {
+	const message = {
+		type: "targetPositionUpdate",
+		position: playerTarget
+	};
+
+	webSocket.send(JSON.stringify(message));
+}
 
 
-webSocket.onmessage = (event) => {
-	console.log(event.data);
-
+function handleWebSocketMessageEvent(event) {
 	const message = JSON.parse(event.data);
 
 	switch (message.type) {
-		case "cellUpdate":
-			playerCell.x = message.cellX;
-			playerCell.y = message.cellY;
+		case "joinGameResponse":
+			handleJoinGameResponseMessage(message);
+			break;
+		case "gameUpdate":
+			handleGameUpdateMessage(message);
 			break;
 	}
 }
 
+
+function handleJoinGameResponseMessage(message) {
+	if (message.joinSuccessful) {
+		playerId = message.playerId;
+
+		setInterval(sendTargetPosition, 1000 / SERVER_TICK_PERIOD);
+	}
+}
+
+
+function handleGameUpdateMessage(message) {
+	cells.clear();
+
+	for (const cell of message.cells) {
+		cells.set(cell.id, cell);
+	}
+}
+
+
+initCanvas();
+connectToServer();
